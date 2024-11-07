c093d138f96dd93cc4080be351bb8eb899b6853f
PriyankaTilekar06 <152593004+PriyankaTilekar06@users.noreply.github.com>
2024-11-07 13:07:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin); // Dynamically set origin
+  res.header("Access-Control-Allow-Origin", req.headers.origin); 
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");

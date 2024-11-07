5f98e14c605fac26a6c4df6ee4b188b6aca5a474
PriyankaTilekar06 <152593004+PriyankaTilekar06@users.noreply.github.com>
2024-11-07 13:19:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,23 +11,24 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Database Connected"))
   .catch((err) => console.log("Database not connected", err));
-const corsOptions = {
-  origin: [
-    'http://localhost:5173', // Local dev server
-    'https://taskmanagement-app-test-frontend.onrender.com', // Frontend on Render
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true, // Allow credentials (cookies) to be sent
-};
 
-app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin); 
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'https://taskmanagement-app-test-frontend.onrender.com'
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+  credentials: true, 
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));

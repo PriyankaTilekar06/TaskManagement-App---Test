6dda5307ca39b825711163c937c75ecc63f04d67
PriyankaTilekar06 <152593004+PriyankaTilekar06@users.noreply.github.com>
2024-11-07 14:41:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const { mongoose } = require("mongoose");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 const app = express();
 
 dotenv.config();
